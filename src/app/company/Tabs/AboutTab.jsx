72d7e3642c8cc8d68fd2a83b77034dd2e30b24b5
Shamil <maashevshamil@gmail.com>
2024-04-08 03:26:32 +0300

    added use client to about tab

--- src/app/company/Tabs/AboutTab.jsx
+++ src/app/company/Tabs/AboutTab.jsx
@@ -1,7 +1,9 @@
+'use clent';
+
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import SectionTitle from '@/components/SectionTitle/SectionTitle';
-// import { Chrono } from 'react-chrono';
+import { Chrono } from 'react-chrono';
 
 export default function AboutTab() {
   const items = [
@@ -120,7 +122,7 @@ export default function AboutTab() {
 
       <SectionTitle text="История развития" />
 
-      {/* <Chrono items={items} mode="VERTICAL_ALTERNATING" disableToolbar /> */}
+      <Chrono items={items} mode="VERTICAL_ALTERNATING" disableToolbar />
     </Box>
   );
 }

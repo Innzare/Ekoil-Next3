bfbe7fc687d288292147976bd1af797b5638b9b3
Shamil <maashevshamil@gmail.com>
2025-01-24 19:37:36 +0500

    corrects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['7cf5aabc-b328-4d8e-96cb-c39c9ba07793.selstorage.ru'] // Добавьте ваш домен сюда
   },
+  output: 'export',
 
   async rewrites() {
     return [

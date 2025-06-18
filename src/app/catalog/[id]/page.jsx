d6e65151ffc836ad1b9f903e519b1c1c91724199
Shamil <maashevshamil@gmail.com>
2025-06-18 20:51:32 +0300

    corrects

--- src/app/catalog/[id]/page.jsx
+++ src/app/catalog/[id]/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-// import { Box, Button, Card, Tab, Tabs, Tooltip, Typography } from '@mui/material';
+import { Box, Button, Card, Tab, Tabs, Tooltip, Typography } from '@mui/material';
 // import { useRouter, useParams, useSearchParams } from 'next/navigation';
 // import SectionTitle from '@/components/SectionTitle/SectionTitle';
 // import ArrowBackIcon from '@mui/icons-material/ArrowBack';
